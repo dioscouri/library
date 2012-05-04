3d7214fec49bd82e9846bbdc8a18978ff18bfb2c
rdiaztushman <rdiaztushman@dioscouri.com>
2012-05-04 21:15:48 +0000

    Formatting change

--- dioscouri.php
+++ dioscouri.php
@@ -193,6 +193,7 @@ class DSC extends JObject
 		
 		$doc = JFactory::getDocument( );
 		$uri = JURI::getInstance( );
+		
 		$js = "Dsc.jbase = '" . $uri->root( ) . "';\n";
 		$doc->addScript( DSC::getURL('js') . 'common.js' );
 		$doc->addScriptDeclaration( $js );
