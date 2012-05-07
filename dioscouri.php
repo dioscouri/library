8625d0bd7bf2eea5e27360b46cf24f8926e2d76e
rdiaztushman <rdiaztushman@dioscouri.com>
2012-05-07 13:51:46 +0000

    Moved dump(), load(), and getClass() into the DSC class. All component config classes (Amigos, Schools, Tags, Tienda, Mediamanager, etc) in the admin/defines.php should now extend DSC instead of JObject

--- dioscouri.php
+++ dioscouri.php
@@ -11,11 +11,11 @@ defined('_JEXEC') or die;
 
 class DSC extends JObject 
 {
-	static $_version 		= '1.0';
+    protected $_name 		= 'dsc';
+    static $_version 		= '1.0';
 	static $_build          = 'r100';
 	static $_versiontype    = 'community';
-	static $_copyrightyear 	= '2011';
-	static $_name 			= 'dsc';
+	static $_copyrightyear 	= '2011';	
 	static $_min_php		= '5.2';
 	
 	/**
@@ -55,9 +55,9 @@ class DSC extends JObject
 	/**
 	 * Get the Name
 	 */
-	public static function getName()
+	public function getName()
 	{
-		return self::$_name;
+	    return $this->get('_name');
 	}
 	
 	/**
@@ -122,23 +122,29 @@ class DSC extends JObject
 	* @param string	$type	The type of URL to return, default 'media'
 	* @return 	string	URL
 	*/
-	public static function getURL( $type = 'media' )
+	public static function getURL( $type = 'media', $com='' )
 	{
+	    $name = 'dioscouri';
+	    if (!empty($com)) {
+	        $app = self::getApp($com);
+	        $name = "com_" . $app;
+	    }
+	    	    
 	    $url = '';
 	
 	    switch ( $type )
 	    {
 	        case 'media':
-	            $url = JURI::root( true ) . '/media/dioscouri/';
+	            $url = JURI::root( true ) . '/media/'.$name.'/';
 	            break;
 	        case 'css':
-	            $url = JURI::root( true ) . '/media/dioscouri/css/';
+	            $url = JURI::root( true ) . '/media/'.$name.'/css/';
 	            break;
 	        case 'images':
-	            $url = JURI::root( true ) . '/media/dioscouri/images/';
+	            $url = JURI::root( true ) . '/media/'.$name.'/images/';
 	            break;
 	        case 'js':
-	            $url = JURI::root( true ) . '/media/dioscouri/js/';
+	            $url = JURI::root( true ) . '/media/'.$name.'/js/';
 	            break;
 	    }
 	
@@ -151,23 +157,29 @@ class DSC extends JObject
 	 * @param 	string	$type	The type of path to return, default 'media'
 	 * @return 	string	Path
 	 */
-	public static function getPath( $type = 'media' )
+	public static function getPath( $type = 'media', $com='' )
 	{
+	    $name = 'dioscouri';
+	    if (!empty($com)) {
+	        $app = self::getApp($com);
+	        $name = "com_" . $app;
+	    }
+	    
 	    $path = '';
 	
 	    switch ( $type )
 	    {
 	        case 'media':
-	            $path = JPATH_SITE . '/media/dioscouri';
+	            $path = JPATH_SITE . '/media/'.$name;
 	            break;
 	        case 'css':
-	            $path = JPATH_SITE . '/media/dioscouri/css';
+	            $path = JPATH_SITE . '/media/'.$name.'/css';
 	            break;
 	        case 'images':
-	            $path = JPATH_SITE . '/media/dioscouri/images';
+	            $path = JPATH_SITE . '/media/'.$name.'/images';
 	            break;
 	        case 'js':
-	            $path = JPATH_SITE . '/media/dioscouri/js';
+	            $path = JPATH_SITE . '/media/'.$name.'/js';
 	            break;
 	    }
 	
@@ -193,19 +205,18 @@ class DSC extends JObject
 		
 		$doc = JFactory::getDocument( );
 		$uri = JURI::getInstance( );
-		
 		$js = "Dsc.jbase = '" . $uri->root( ) . "';\n";
 		$doc->addScript( DSC::getURL('js') . 'common.js' );
 		$doc->addScriptDeclaration( $js );
 				
 		return true;
 	}
-
+	
 	/**
-	 * Adds the Highcharts library files to the autoloader
-	 * and adds the highcharts js file to the stack
-	 * 
-	 */
+	* Adds the Highcharts library files to the autoloader
+	* and adds the highcharts js file to the stack
+	*
+	*/
 	public static function loadHighcharts()
 	{
 	    jimport('dioscouri.highroller.highroller.highroller');
@@ -218,8 +229,255 @@ class DSC extends JObject
 	    jimport('dioscouri.highroller.highroller.highrollerscatterchart');
 	    jimport('dioscouri.highroller.highroller.highrollerseriesdata');
 	    jimport('dioscouri.highroller.highroller.highrollersplinechart');
-	    
+	     
 	    JHTML::_( 'script', 'highcharts.js', 'libraries/dioscouri/highroller/highcharts/' );
+	}
+	
+	/**
+	* Method to dump the structure of a variable for debugging purposes
+	*
+	* @param	mixed	A variable
+	* @param	boolean	True to ensure all characters are htmlsafe
+	* @return	string
+	* @since	1.5
+	* @static
+	*/
+	public static function dump( $var, $ignore_underscore = true, $htmlSafe = true )
+	{
+	    if (!$ignore_underscore)
+	    {
+	        $result = print_r( $var, true );
+	        return '<pre>'.( $htmlSafe ? htmlspecialchars( $result ) : $result).'</pre>';
+	    }
+	     
+	    if (!is_object($var) && !is_array($var))
+	    {
+	        $result = print_r( $var, true );
+	        return '<pre>'.( $htmlSafe ? htmlspecialchars( $result ) : $result).'</pre>';
+	    }
+	     
+	    // TODO do a recursive remove of underscored keys
+	     
+	    if (is_object($var))
+	    {
+	        $keys = get_object_vars($var);
+	        foreach ($keys as $key=>$value)
+	        {
+	            if (substr($key, 0, 1) == '_')
+	            {
+	                unset($var->$key);
+	            }
+	            else
+	            {
+	                if (is_object($var->$key))
+	                {
+	                    $sub_keys = get_object_vars($var->$key);
+	                    foreach ($sub_keys as $sub_key=>$sub_key_value)
+	                    {
+	                        if (substr($sub_key, 0, 1) == '_')
+	                        {
+	                            unset($var->$key->$sub_key);
+	                        }
+	                    }
+	                }
+	                elseif (is_array($var->$key))
+	                {
+	                    foreach ($var->$key as $sub_key=>$sub_key_value)
+	                    {
+	                        if (substr($sub_key, 0, 1) == '_')
+	                        {
+	                            unset($var->$key[$sub_key]);
+	                        }
+	                    }
+	                }
+	            }
+	             
+	             
+	        }
+	        $result = @print_r( $var, true );
+	        return '<pre>'.( $htmlSafe ? htmlspecialchars( $result ) : $result).'</pre>';
+	    }
+	     
+	    if (is_array($var))
+	    {
+	        foreach ($var as $key=>$value)
+	        {
+	            if (substr($key, 0, 1) == '_')
+	            {
+	                unset($var[$key]);
+	            }
+	            else
+	            {
+	                if (is_object($var[$key]))
+	                {
+	                    $sub_keys = get_object_vars($var[$key]);
+	                    foreach ($sub_keys as $sub_key=>$sub_key_value)
+	                    {
+	                        if (substr($sub_key, 0, 1) == '_')
+	                        {
+	                            unset($var[$key]->$sub_key);
+	                        }
+	                    }
+	                }
+	                elseif (is_array($var[$key]))
+	                {
+	                    foreach ($var[$key] as $sub_key=>$sub_key_value)
+	                    {
+	                        if (substr($sub_key, 0, 1) == '_')
+	                        {
+	                            unset($var[$key][$sub_key]);
+	                        }
+	                    }
+	                }
+	            }
+	        }
+	        $result = @print_r( $var, true );
+	        return '<pre>'.( $htmlSafe ? htmlspecialchars( $result ) : $result).'</pre>';
+	    }
+	}
+	
+	/**
+	 * Method to intelligently load class files in the framework
+	 *
+	 * @param string $classname   The class name
+	 * @param string $filepath    The filepath ( dot notation )
+	 * @param array  $options
+	 * @return boolean
+	 */
+	public static function load( $classname, $filepath='library', $options=array( 'site'=>'site', 'type'=>'libraries', 'ext'=>'dioscouri' ) )
+	{
+	    $classname = strtolower( $classname );
+	    if(version_compare(JVERSION,'1.6.0','ge')) {
+	        // Joomla! 1.6+ code here
+	        $classes = JLoader::getClassList();
+	    } else {
+	        // Joomla! 1.5 code here
+	        $classes = JLoader::register();
+	    }
+	
+	    if ( class_exists($classname) || array_key_exists( $classname, $classes ) )
+	    {
+	        // echo "$classname exists<br/>";
+	        return true;
+	    }
+	
+	    static $paths;
+	
+	    if (empty($paths))
+	    {
+	        $paths = array();
+	    }
+	
+	    if (empty($paths[$classname]) || !is_file($paths[$classname]))
+	    {
+	        // find the file and set the path
+	        if (!empty($options['base']))
+	        {
+	            $base = $options['base'];
+	        }
+	        else
+	        {
+	            // recreate base from $options array
+	            switch ($options['site'])
+	            {
+	                case "site":
+	                    $base = JPATH_SITE.DS;
+	                    break;
+	                default:
+	                    $base = JPATH_ADMINISTRATOR.DS;
+	                break;
+	            }
+	
+	            $base .= (!empty($options['type'])) ? $options['type'].DS : '';
+	            $base .= (!empty($options['ext'])) ? $options['ext'].DS : '';
+	        }
+	
+	        $paths[$classname] = $base.str_replace( '.', DS, $filepath ).'.php';
+	    }
+	
+	    // if invalid path, return false
+	    if (!is_file($paths[$classname]))
+	    {
+	        // echo "file does not exist<br/>";
+	        return false;
+	    }
+	
+	    // if not registered, register it
+	    if ( !array_key_exists( $classname, $classes ) )
+	    {
+	        // echo "$classname not registered, so registering it<br/>";
+	        JLoader::register( $classname, $paths[$classname] );
+	        return true;
+	    }
+	    return false;
+	}
+	
+	/**
+	 * Intelligently loads instances of classes in framework
+	 *
+	 * Usage: $object = DSC::getClass( 'DSCHelperCarts', 'helpers.carts' );
+	 * Usage: $suffix = DSC::getClass( 'DSCHelperCarts', 'helpers.carts' )->getSuffix();
+	 * Usage: $categories = DSC::getClass( 'DSCSelect', 'select' )->category( $selected );
+	 *
+	 * @param string $classname   The class name
+	 * @param string $filepath    The filepath ( dot notation )
+	 * @param array  $options
+	 * @return object of requested class (if possible), else a new JObject
+	 */
+	public static function getClass( $classname, $filepath='library', $options=array( 'site'=>'site', 'type'=>'libraries', 'ext'=>'dioscouri' )  )
+	{
+	    if (self::load( $classname, $filepath, $options ))
+	    {
+	        $instance = new $classname();
+	        return $instance;
+	    }
+	
+	    $instance = new JObject();
+	    return $instance;
+	}
+	
+	/**
+	* Retrieves the data
+	* @return array Array of objects containing the data from the database
+	*/
+	public function getData() 
+	{
+	    // load the data if it doesn't already exist
+	    if (empty( $this->_data )) {
+	        $database = JFactory::getDBO();
+	        $query = $this->_buildQuery();
+	        $database->setQuery( $query );
+	        $this->_data = $database->loadObjectList();
+	    }
+	
+	    return $this->_data;
+	}
+	
+	/**
+	 * Set Variables
+	 *
+	 * @acces	public
+	 * @return	object
+	 */
+	public function setVariables() 
+	{
+	    $success = false;
+
+	    if ( $data = $this->getData() )
+	    {
+	        for ($i=0; $i<count($data); $i++)
+	        {
+	            $title = $data[$i]->config_name;
+	            $value = $data[$i]->value;
+	            if (!empty($title)) {
+	                $this->$title = $value;
+	            }
+	        }
+
+	        $success = true;
+	    }
+
+	    return $success;
 	}
 }
 ?>
