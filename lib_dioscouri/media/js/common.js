20871186cf0e39bc17165fdb82b0374b9518d02e
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2012-11-05 02:01:52 -0500

    Need to let apps decide when to trigger the formValidation

--- lib_dioscouri/media/js/common.js
+++ lib_dioscouri/media/js/common.js
@@ -83,50 +83,42 @@ Dsc.formValidation = function( url, container, task, form, doModal, msg, onCompl
 {
 	if (doModal != false) { Dsc.newModal(msg); }
 	
-    if (task == 'save' || task == 'apply' || task == 'savenew') 
+    // loop through form elements and prepare an array of objects for passing to server
+    var str = new Array();
+    for(i=0; i<form.elements.length; i++)
     {
-        // loop through form elements and prepare an array of objects for passing to server
-        var str = new Array();
-        for(i=0; i<form.elements.length; i++)
-        {
-            if (form.elements[i].name) {
-                postvar = {
-                        name : form.elements[i].name,
-                        value : form.elements[i].value,
-                        checked : form.elements[i].checked,
-                        id : form.elements[i].id
-                    };
-                str[i] = postvar;
-            }
+        if (form.elements[i].name) {
+            postvar = {
+                    name : form.elements[i].name,
+                    value : form.elements[i].value,
+                    checked : form.elements[i].checked,
+                    id : form.elements[i].id
+                };
+            str[i] = postvar;
         }
-        
-        // execute request to server
-        var a = new Request({
-            url: url,
-            method:"post",
-            data:{"elements":JSON.encode(str)},
-            onSuccess: function(response){
-                var resp = JSON.decode(response, false);
-                if (resp.error != '1')
-                {
-                    if (typeof onCompleteFunction == 'function') {
-                        onCompleteFunction();
-                    }
-                    form.task.value = task;
-                    form.submit();
-                } else {
-                    if (document.id(container)) { document.id(container).set( 'html', resp.msg); }
+    }
+    
+    // execute request to server
+    var a = new Request({
+        url: url,
+        method:"post",
+        data:{"elements":JSON.encode(str)},
+        onSuccess: function(response){
+            var resp = JSON.decode(response, false);
+            if (resp.error != '1')
+            {
+                if (typeof onCompleteFunction == 'function') {
+                    onCompleteFunction();
                 }
-                if (doModal != false) { (function() { document.body.removeChild( document.getElementById('dscModal') ); }).delay(500); }
+                form.task.value = task;
+                form.submit();
+            } else {
+                if (document.id(container)) { document.id(container).set( 'html', resp.msg); }
             }
-        }).send();
-        
-    }
-        else 
-    {
-        form.task.value = task;
-        form.submit();
-    }
+            if (doModal != false) { (function() { document.body.removeChild( document.getElementById('dscModal') ); }).delay(500); }
+        }
+    }).send();
+
 }
 
 /**
