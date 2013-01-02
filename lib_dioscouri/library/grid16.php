3b3d93ecc5ffd555ed0a24ead672b35cb297dd3f
AmmoniteNetworks <chris@ammonitenetworks.com>
2013-01-02 12:09:14 -0800

    adding the button classes to base grid

--- lib_dioscouri/library/grid16.php
+++ lib_dioscouri/library/grid16.php
@@ -14,7 +14,97 @@ defined('_JEXEC') or die('Restricted access');
 require_once( JPATH_SITE.DS.'libraries'.DS.'joomla'.DS.'html'.DS.'html'.DS.'grid.php' );
 
 class DSCGrid extends JHTMLGrid
-{
+{	
+
+	/**
+     * A boolean radiolist that uses bootstrap
+     *  
+     * @param unknown_type $name
+     * @param unknown_type $attribs
+     * @param unknown_type $selected
+     * @param unknown_type $yes
+     * @param unknown_type $no
+     * @param unknown_type $id
+     * @return string
+     */
+	public static function btbooleanlist($name, $attribs = null, $selected = null, $yes = 'JYES', $no = 'JNO', $id = false)
+	{
+	    JHTML::_('script', 'bootstrapped-advanced-ui.js', 'media/dioscouri/js/');
+	    JHTML::_('stylesheet', 'bootstrapped-advanced-ui.css', 'media/dioscouri/css/');
+	    $arr = array(JHtml::_('select.option', '0', JText::_($no)), JHtml::_('select.option', '1', JText::_($yes)));
+	    $html = '<div class="control-group"><div class="controls"><fieldset id="'.$name.'" class="radio btn-group">';
+	    $html .=  DSCGrid::btradiolist( $arr, $name, $attribs, 'value', 'text', (int) $selected, $id);
+	    $html .= '</fieldset></div></div>';
+
+	    return $html;
+	}
+	
+	/**
+	 * A standard radiolist that uses bootstrap
+	 * 
+	 * @param unknown_type $data
+	 * @param unknown_type $name
+	 * @param unknown_type $attribs
+	 * @param unknown_type $optKey
+	 * @param unknown_type $optText
+	 * @param unknown_type $selected
+	 * @param unknown_type $idtag
+	 * @param unknown_type $translate
+	 * @return string
+	 */
+	public static function btradiolist($data, $name, $attribs = null, $optKey = 'value', $optText = 'text', $selected = null, $idtag = false, $translate = false)
+	{
+	    reset($data);
+	    $html = '';
+
+	    if (is_array($attribs))
+	    {
+	        $attribs = JArrayHelper::toString($attribs);
+	    }
+
+	    $id_text = $idtag ? $idtag : $name;
+
+	    foreach ($data as $obj)
+	    {
+	        $k = $obj->$optKey;
+	        $t = $translate ? JText::_($obj->$optText) : $obj->$optText;
+	        $id = (isset($obj->id) ? $obj->id : null);
+
+	        $extra = '';
+	        $extra .= $id ? ' id="' . $obj->id . '"' : '';
+	        if (is_array($selected))
+	        {
+	            foreach ($selected as $val)
+	            {
+	                $k2 = is_object($val) ? $val->$optKey : $val;
+	                if ($k == $k2)
+	                {
+	                    $extra .= ' selected="selected"';
+	                    break;
+	                }
+	            }
+	        }
+	        else
+	        {
+	            $extra .= ((string) $k == (string) $selected ? ' checked="checked"' : '');
+	        }
+	        
+	        $active ='';
+	        if(!empty($k)) {
+	            $active = 'active';
+	        }
+	        
+	        $html .= "\n\t" . '<input type="radio" name="' . $name . '"' . ' id="' . $id_text . $k . '" value="' . $k . '"' . ' ' . $extra . ' '
+	        . $attribs . '/>' . "\n\t" . '<label for="' . $id_text . $k . '"' . ' id="' . $id_text . $k . '-lbl" class="btn">' . $t
+	        . '</label>';
+	    }
+	    
+	    $html .= "\n";
+	    
+	    return $html;
+	}
+
+
 	/**
 	 * @param	string	The link title
 	 * @param	string	The order field for the column
